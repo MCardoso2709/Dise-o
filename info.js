83a16f9fdbb6ce39a4aa66e09f3add797809b008
juanduran02 <100865574+juanduran02@users.noreply.github.com>
2024-02-04 10:32:40 -0500

    Delete info.js

--- info.js
+++
@@ -1,7 +0,0 @@
-function mostrarInfo(elemento) {
-    elemento.querySelector('.info').style.display = 'block';
-}
-
-function ocultarInfo(elemento) {
-    elemento.querySelector('.info').style.display = 'none';
-}
