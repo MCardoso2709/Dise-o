d94a69d4942b6fdfd1f4595533d34cebe1a7d4aa
robcast03 <158825707+robcast03@users.noreply.github.com>
2024-02-09 09:10:59 -0500

    Update info.js

--- js/info.js
+++ js/info.js
@@ -5,6 +5,15 @@ function mostrarInfo(elemento) {
 function ocultarInfo(elemento) {
     elemento.querySelector('.info').style.display = 'none';
 }
+document.addEventListener("DOMContentLoaded", function() {
+    // Selecciona el botón por su id
+    var botonPlanos = document.getElementById("botonPlanos");
+
+    // Agrega un evento de clic al botón que llama a la función paginaPlanos
+    botonPlanos.addEventListener("click", function() {
+        paginaPlanos();
+    });
+});
 function paginaPlanos(){
     console.log('Redireccionando a galleryPlanos.html');
     window.location.href = 'galleryPlanos.html';
