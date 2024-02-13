18dd357daf87130a51f6405e17138adaa7094440
robcast03 <castro.robert2003@gmail.com>
2024-02-12 21:53:22 -0500

    prueba authentication

---
+++ js/authentication.js
@@ -0,0 +1,1 @@
+import 'js/signInform.js'
---
+++ js/signInForm.js
@@ -0,0 +1,18 @@
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js"
+import{auth} from './firebaseSDK.js'
+
+const signInForm =document.querySelector('#login-form')
+
+signInForm.addEventListener('submit', async (e)=>{
+    e.preventDefault()
+    const email = signInForm['login-email'].value
+    const password = signInForm['login-password'].value
+    console.log(email,password)
+    try {
+        const credentials = await signInWithEmailAndPassword(auth,email,password)
+        console.log(credentials)
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
